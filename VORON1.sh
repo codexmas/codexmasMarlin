f41b9ad64975226049135c2c0a3fd8b8c64875ea
Gord Christmas <codexmas@gmail.com>
2016-10-17 07:52:52 -0700

    RC7 Config, MBL not working yet except for manual mode

--- VORON1.sh
+++ VORON1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Main config changes
 sed -i '' \
- -e '\|#define STRING_CONFIG_H_AUTHOR|s|none, default config|codexmas VORON:1|' \
- -e 's|//#define CUSTOM_MACHINE_NAME "3D Printer"|#define CUSTOM_MACHINE_NAME "VORON:1"|' \
+ -e '\|#define STRING_CONFIG_H_AUTHOR|s|none, default config|codexmas VORON#4|' \
+ -e 's|//#define CUSTOM_MACHINE_NAME "3D Printer"|#define CUSTOM_MACHINE_NAME "VORON#4"|' \
+ -e '\|#define POWER_SUPPLY|s|0|1|' \
  -e 's|//#define PS_DEFAULT_OFF|#define PS_DEFAULT_OFF|' \
  -e 's|#define TEMP_SENSOR_0.*|#define TEMP_SENSOR_0 5|' \
  -e 's|#define TEMP_SENSOR_BED.*|#define TEMP_SENSOR_BED 1|' \
@@ -11,33 +12,20 @@ sed -i '' \
  -e '\|#define MAX_BED_POWER|s|255|190|' \
  -e 's|//#define COREXY|#define COREXY|' \
  -e 's|//#define ENDSTOPPULLUP_ZMIN|#define ENDSTOPPULLUP_ZMIN|' \
- -e '\|Z_MIN_ENDSTOP_INVERTING|s|false|true|' \
- -e '\|Z_MIN_PROBE_ENDSTOP_INVERTING|s|false|true|' \
  -e '\|INVERT_Y_DIR|s|true|false|' \
  -e '\|X_MAX_POS|s|200|228|' \
  -e '\|Y_MAX_POS|s|200|228|' \
  -e '\|Z_MAX_POS|s|200|228|' \
- -e 's|//#define AUTO_BED_LEVELING_FEATURE|#define AUTO_BED_LEVELING_FEATURE|' \
- -e 's|//#define DEBUG_LEVELING_FEATURE|#define DEBUG_LEVELING_FEATURE|' \
- -e '\|LEFT_PROBE_BED_POSITION|s|15|20|' \
- -e '\|RIGHT_PROBE_BED_POSITION|s|170|180|' \
- -e '\|FRONT_PROBE_BED_POSITION|s|20|20|' \
- -e '\|BACK_PROBE_BED_POSITION|s|170|180|' \
- -e '\|AUTO_BED_LEVELING_GRID_POINTS|s|2|3|' \
- -e '\|ABL_PROBE_PT_1_X|s|15|20|' \
- -e '\|ABL_PROBE_PT_1_Y|s|180|190|' \
- -e '\|ABL_PROBE_PT_2_X|s|15|20|' \
- -e '\|ABL_PROBE_PT_2_Y|s|20|20|' \
- -e '\|ABL_PROBE_PT_3_X|s|170|208|' \
- -e '\|ABL_PROBE_PT_3_Y|s|20|20|' \
+ -e 's|//#define BLTOUCH|#define BLTOUCH|' \
  -e '\|X_PROBE_OFFSET_FROM_EXTRUDER|s|10|0|' \
- -e '\|Y_PROBE_OFFSET_FROM_EXTRUDER|s|10|-45|' \
+ -e '\|Y_PROBE_OFFSET_FROM_EXTRUDER|s|10|-42|' \
  -e '\|Z_PROBE_OFFSET_FROM_EXTRUDER|s|10|0|' \
- -e '\|XY_TRAVEL_SPEED|s|8000|15000|' \
- -e '\|Z_RAISE_BEFORE_PROBING|s|15|2|' \
- -e '\|Z_RAISE_BETWEEN_PROBINGS|s|15|2|' \
- -e '\|Z_RAISE_AFTER_PROBING|s|15|5|' \
- -e 's|//#define FIX_MOUNTED_PROBE|#define FIX_MOUNTED_PROBE|' \
+ -e '\|XY_PROBE_SPEED|s|8000|15000|' \
+ -e '\|Z_CLEARANCE_DEPLOY_PROBE|s|10|5|' \
+ -e '\|Z_CLEARANCE_BETWEEN_PROBES|s|5|2|' \
+ -e 's|//#define MESH_BED_LEVELING|#define MESH_BED_LEVELING|' \
+ -e '\|#define MESH_INSET|s|10|18|' \
+ -e 's|//#define MESH_G28_REST_ORIGIN|#define MESH_G28_REST_ORIGIN|' \
  -e 's|//#define Z_SAFE_HOMING|#define Z_SAFE_HOMING|' \
  -e 's|//#define EEPROM_SETTINGS|#define EEPROM_SETTINGS|' \
  -e 's|//#define SDSUPPORT|#define SDSUPPORT|' \

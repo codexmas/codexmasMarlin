f1d74057cdbc80e2c0775a0d3c6a84c656fb3d13
Gord Christmas <codexmas@gmail.com>
2018-01-31 07:44:38 -0800

    Updated to 1.1.8 firmware

--- VORON1.sh
+++ VORON1.sh
@@ -3,6 +3,7 @@
 sed -i '' \
  -e '\|#define STRING_CONFIG_H_AUTHOR|s|none, default config|codexmas VORON#4|' \
  -e 's|//#define CUSTOM_MACHINE_NAME "3D Printer"|#define CUSTOM_MACHINE_NAME "VORON#4"|' \
+ -e '\|DEFAULT_NOMINAL_FILAMENT_DIA|s|3.0|2.85|' \
  -e '\|#define POWER_SUPPLY|s|0|1|' \
  -e 's|//#define PS_DEFAULT_OFF|#define PS_DEFAULT_OFF|' \
  -e 's|#define TEMP_SENSOR_0.*|#define TEMP_SENSOR_0 5|' \
@@ -17,6 +18,8 @@ sed -i '' \
  -e '\|Y_MAX_POS|s|200|228|' \
  -e '\|Z_MAX_POS|s|200|228|' \
  -e 's|//#define BLTOUCH|#define BLTOUCH|' \
+ -e '\|#define X_BED_SIZE|s|200|228|' \
+ -e '\|#define Y_BED_SIZE|s|200|228|' \
  -e '\|X_PROBE_OFFSET_FROM_EXTRUDER|s|10|0|' \
  -e '\|Y_PROBE_OFFSET_FROM_EXTRUDER|s|10|-42|' \
  -e '\|Z_PROBE_OFFSET_FROM_EXTRUDER|s|10|0|' \
@@ -24,7 +27,14 @@ sed -i '' \
  -e '\|Z_CLEARANCE_DEPLOY_PROBE|s|10|5|' \
  -e '\|Z_CLEARANCE_BETWEEN_PROBES|s|5|2|' \
  -e 's|//#define AUTO_BED_LEVELING_UBL|#define AUTO_BED_LEVELING_UBL|' \
- -e '\|#define UBL_MESH_INSET|s|1|4|' \
+ -e 's|#define MESH_INSET 1              // Mesh inset margin on print area|#define MESH_INSET 10|' \
+ -e '\|#define GRID_MAX_POINTS_X|s|10|8|' \
+ -e '\|#define UBL_PROBE_PT_1_X|s|39|114|' \
+ -e '\|#define UBL_PROBE_PT_1_Y|s|180|50|' \
+ -e '\|#define UBL_PROBE_PT_2_X|s|39|50|' \
+ -e '\|#define UBL_PROBE_PT_2_Y|s|20|180|' \
+ -e '\|#define UBL_PROBE_PT_3_X|s|180|200|' \
+ -e '\|#define UBL_PROBE_PT_3_Y|s|20|180|' \
  -e 's|//#define Z_SAFE_HOMING|#define Z_SAFE_HOMING|' \
  -e 's|//#define EEPROM_SETTINGS|#define EEPROM_SETTINGS|' \
  -e 's|//#define SDSUPPORT|#define SDSUPPORT|' \
@@ -32,8 +42,6 @@ sed -i '' \
  ~/GIT/Marlin/Marlin/Configuration.h
 
  sed -i '' \
- -e 's|#define UBL_MESH_MIN_X (X_MIN_POS + UBL_MESH_INSET)|#define\UBL_MESH_MIN_X (X_MIN_POS + 6 + UBL_MESH_INSET)|' \
- -e 's|#define UBL_MESH_MIN_Y (Y_MIN_POS + UBL_MESH_INSET)|#define UBL_MESH_MIN_Y (Y_MIN_POS + 45 + UBL_MESH_INSET)|' \
  -e '\|#define THERMAL_PROTECTION_HYSTERESIS|s|4|10|' \
  -e '\|#define THERMAL_PROTECTION_BED_HYSTERESIS|s|2|6|' \
  ~/GIT/Marlin/Marlin/Configuration_adv.h
